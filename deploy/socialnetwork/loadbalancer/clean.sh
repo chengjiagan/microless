27bcc9aa9689896833ae5c6aa5749ad505bd521f
Jiagan Cheng <chnegjiagan@gmail.com>
2023-04-20 16:15:48 +0800

    [deploy] add loadbalancer deployment for social-network

---
+++ deploy/socialnetwork/loadbalancer/clean.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# this script is just for reference, please do not run it directly
+
+kubectl delete ns social-network-lb-test
+kubectl delete ns social-network-lb-vm
+kubectl delete ns social-network-lb-kn
+kubectl delete ns social-network-lb
