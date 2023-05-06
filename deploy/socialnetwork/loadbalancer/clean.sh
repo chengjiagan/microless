e8bfd6d5a658a2e3191ebcdd0203d74938f6af2a
Jiagan Cheng <chnegjiagan@gmail.com>
2023-05-06 14:57:41 +0800

    [deploy] update socialnetwork to new loadbalancer and version 0.2.0

--- deploy/socialnetwork/loadbalancer/clean.sh
+++ deploy/socialnetwork/loadbalancer/clean.sh
@@ -2,7 +2,5 @@
 
 # this script is just for reference, please do not run it directly
 
-kubectl delete ns social-network-lb-test
-kubectl delete ns social-network-lb-vm
-kubectl delete ns social-network-lb-kn
-kubectl delete ns social-network-lb
+kubectl delete ns social-network-test
+kubectl delete ns social-network
--- deploy/socialnetwork/loadbalancer/deploy.sh
+++ deploy/socialnetwork/loadbalancer/deploy.sh
@@ -5,8 +5,13 @@
 # Create socialnetwork namespace
 kubectl create -f namespace.yaml
 
-# Install vm-services
-kubectl create -f vm-service/config.yaml
+# Install service config
+kubectl create -f config.yaml
+
+# Install restful gateway
+kubectl create -f gateway.yaml
+
+# Install vm services
 kubectl create -f vm-service/post-storage-service.yaml
 kubectl create -f vm-service/user-timeline-service.yaml
 kubectl create -f vm-service/user-service.yaml
@@ -17,10 +22,8 @@ kubectl create -f vm-service/url-shorten-service.yaml
 kubectl create -f vm-service/user-mention-service.yaml
 kubectl create -f vm-service/text-service.yaml
 kubectl create -f vm-service/compose-post-service.yaml
-kubectl create -f vm-service/restful-gateway-service.yaml
 
-# Install kn-services
-kubectl create -f kn-service/config.yaml
+# Install serverless services
 kubectl create -f kn-service/post-storage-service.yaml
 kubectl create -f kn-service/user-timeline-service.yaml
 kubectl create -f kn-service/user-service.yaml
@@ -31,36 +34,18 @@ kubectl create -f kn-service/url-shorten-service.yaml
 kubectl create -f kn-service/user-mention-service.yaml
 kubectl create -f kn-service/text-service.yaml
 kubectl create -f kn-service/compose-post-service.yaml
-kubectl create -f kn-service/restful-gateway-service.yaml
-
-# Install ingress
-kubectl create -f ingress.yaml
 
 # Install HPA objects
-kubectl create -f hpa/post-storage-hpa.yaml
-kubectl create -f hpa/user-timeline-hpa.yaml
-kubectl create -f hpa/user-hpa.yaml
-kubectl create -f hpa/social-graph-hpa.yaml
-kubectl create -f hpa/home-timeline-hpa.yaml
-kubectl create -f hpa/media-hpa.yaml
-kubectl create -f hpa/url-shorten-hpa.yaml
-kubectl create -f hpa/user-mention-hpa.yaml
-kubectl create -f hpa/text-hpa.yaml
-kubectl create -f hpa/compose-post-hpa.yaml
-kubectl create -f hpa/restful-gateway-hpa.yaml
-
-# Install load balancer
-kubectl create -f loadbalancer/post-storage-lb.yaml
-kubectl create -f loadbalancer/user-timeline-lb.yaml
-kubectl create -f loadbalancer/user-lb.yaml
-kubectl create -f loadbalancer/social-graph-lb.yaml
-kubectl create -f loadbalancer/home-timeline-lb.yaml
-kubectl create -f loadbalancer/media-lb.yaml
-kubectl create -f loadbalancer/url-shorten-lb.yaml
-kubectl create -f loadbalancer/user-mention-lb.yaml
-kubectl create -f loadbalancer/text-lb.yaml
-kubectl create -f loadbalancer/compose-post-lb.yaml
-kubectl create -f loadbalancer/restful-gateway-lb.yaml
+# kubectl create -f hpa/post-storage-hpa.yaml
+# kubectl create -f hpa/user-timeline-hpa.yaml
+# kubectl create -f hpa/user-hpa.yaml
+# kubectl create -f hpa/social-graph-hpa.yaml
+# kubectl create -f hpa/home-timeline-hpa.yaml
+# kubectl create -f hpa/media-hpa.yaml
+# kubectl create -f hpa/url-shorten-hpa.yaml
+# kubectl create -f hpa/user-mention-hpa.yaml
+# kubectl create -f hpa/text-hpa.yaml
+# kubectl create -f hpa/compose-post-hpa.yaml
 
 # # Wait for vm-services
 # sleep 30
