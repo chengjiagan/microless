2d6b793197d8e817dd38b17dfc1573f3244559f3
Jiagan Cheng <chnegjiagan@gmail.com>
2023-05-06 14:56:35 +0800

    [socialnetwork] use new loadbalancer and add back gateway, new release 0.2.0

---
+++ socialnetwork/src/gateway/cmd/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"microless/socialnetwork/proto/composepost"
+	"microless/socialnetwork/proto/hometimeline"
+	"microless/socialnetwork/proto/socialgraph"
+	"microless/socialnetwork/proto/user"
+	"microless/socialnetwork/proto/usertimeline"
+	"microless/socialnetwork/utils"
+	"net/http"
+	"os"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.uber.org/zap"
+)
+
+var configPath = flag.String("config", os.Getenv("SERVICE_CONFIG"), "path to config file")
+
+func main() {
+	// setup logger
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	flag.Parse()
+	config, err := utils.ParseConfig(*configPath)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// setup opentelemetry
+	logger.Info("connect to jaeger")
+	tp, err := utils.NewTracerProvider(ctx, "RestfulGateway", config.Otel)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func() {
+		if err := utils.Shutdown(tp, ctx); err != nil {
+			logger.Fatal(err.Error())
+		}
+	}()
+
+	mux := runtime.NewServeMux()
+	// connect to ComposerPost grpc server
+	conn, err := utils.NewConn(config.Service.ComposePost)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	err = composepost.RegisterComposePostServiceHandler(ctx, mux, conn)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	// connect to HomeTimeline grpc server
+	conn, err = utils.NewConn(config.Service.HomeTimeline)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	err = hometimeline.RegisterHomeTimelineServiceHandler(ctx, mux, conn)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	// connect to SocialGraph grpc server
+	conn, err = utils.NewConn(config.Service.SocialGraph)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	err = socialgraph.RegisterSocialGraphServiceHandler(ctx, mux, conn)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	// connect to User grpc server
+	conn, err = utils.NewConn(config.Service.User)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	err = user.RegisterUserServiceHandler(ctx, mux, conn)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	// connect to UserTimeline grpc server
+	conn, err = utils.NewConn(config.Service.UserTimeline)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	err = usertimeline.RegisterUserTimelineServiceHandler(ctx, mux, conn)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	// setup opentelemetry http handler
+	handler := otelhttp.NewHandler(mux, "gateway")
+
+	logger.Info("start server")
+	err = http.ListenAndServe(config.Grpc, handler)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+}
--- socialnetwork/src/utils/grpc_utils.go
+++ socialnetwork/src/utils/grpc_utils.go
@@ -4,14 +4,20 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+
+	"microless/loadbalancer"
 )
 
 func NewConn(address string) (*grpc.ClientConn, error) {
+	lb := loadbalancer.NewClientLB(address)
+
 	conn, err := grpc.Dial(
 		address,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor()),
-		grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor()),
+		grpc.WithChainUnaryInterceptor(
+			lb.UnaryClientInterceptor(),
+			otelgrpc.UnaryClientInterceptor(),
+		),
 	)
 	if err != nil {
 		return nil, err
@@ -21,8 +27,12 @@ func NewConn(address string) (*grpc.ClientConn, error) {
 }
 
 func NewGRPCServer() *grpc.Server {
+	lb := loadbalancer.NewServerLB()
+
 	return grpc.NewServer(
-		grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor()),
-		grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()),
+		grpc.ChainUnaryInterceptor(
+			lb.UnaryServerInterceptor(),
+			otelgrpc.UnaryServerInterceptor(),
+		),
 	)
 }
