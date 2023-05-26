b2f83df028b61eb5df4cf0bb2f7fdaffb8db8b89
Jiagan Cheng <chnegjiagan@gmail.com>
2023-05-26 14:04:46 +0800

    [deploy] update socialnetwork loadbalancer deploy

--- deploy/socialnetwork/loadbalancer/clean.sh
+++ deploy/socialnetwork/loadbalancer/clean.sh
@@ -2,5 +2,4 @@
 
 # this script is just for reference, please do not run it directly
 
-kubectl delete ns social-network-test
 kubectl delete ns social-network
--- deploy/socialnetwork/loadbalancer/deploy.sh
+++ deploy/socialnetwork/loadbalancer/deploy.sh
@@ -2,53 +2,42 @@
 
 # this script is just for reference, please do not run it directly
 
-# Create socialnetwork namespace
-kubectl create -f namespace.yaml
+# Install socialnetwork namespace
+kubectl apply -f namespace.yaml
 
 # Install service config
-kubectl create -f config.yaml
+kubectl apply -f config.yaml
 
-# Install restful gateway
-kubectl create -f gateway.yaml
-
-# Install vm services
-kubectl create -f vm-service/post-storage-service.yaml
-kubectl create -f vm-service/user-timeline-service.yaml
-kubectl create -f vm-service/user-service.yaml
-kubectl create -f vm-service/social-graph-service.yaml
-kubectl create -f vm-service/home-timeline-service.yaml
-kubectl create -f vm-service/media-service.yaml
-kubectl create -f vm-service/url-shorten-service.yaml
-kubectl create -f vm-service/user-mention-service.yaml
-kubectl create -f vm-service/text-service.yaml
-kubectl create -f vm-service/compose-post-service.yaml
+SERVICES=(post-storage user-timeline user social-graph home-timeline media url-shorten user-mention text compose-post)
 
 # Install serverless services
-kubectl create -f kn-service/post-storage-service.yaml
-kubectl create -f kn-service/user-timeline-service.yaml
-kubectl create -f kn-service/user-service.yaml
-kubectl create -f kn-service/social-graph-service.yaml
-kubectl create -f kn-service/home-timeline-service.yaml
-kubectl create -f kn-service/media-service.yaml
-kubectl create -f kn-service/url-shorten-service.yaml
-kubectl create -f kn-service/user-mention-service.yaml
-kubectl create -f kn-service/text-service.yaml
-kubectl create -f kn-service/compose-post-service.yaml
+for s in ${SERVICES[@]}; do
+    kubectl apply -f serverless-service/$s-service.yaml
+done
 
-# Install HPA objects
-# kubectl create -f hpa/post-storage-hpa.yaml
-# kubectl create -f hpa/user-timeline-hpa.yaml
-# kubectl create -f hpa/user-hpa.yaml
-# kubectl create -f hpa/social-graph-hpa.yaml
-# kubectl create -f hpa/home-timeline-hpa.yaml
-# kubectl create -f hpa/media-hpa.yaml
-# kubectl create -f hpa/url-shorten-hpa.yaml
-# kubectl create -f hpa/user-mention-hpa.yaml
-# kubectl create -f hpa/text-hpa.yaml
-# kubectl create -f hpa/compose-post-hpa.yaml
+# Install vm services
+for s in ${SERVICES[@]}; do
+    kubectl apply -f vm-service/$s-service.yaml
+done
 
-# # Wait for vm-services
+# Install services
+for s in ${SERVICES[@]}; do
+    kubectl apply -f service/$s-service.yaml
+done
+
+# Install restful gateway
+kubectl apply -f gateway.yaml
+
+# Install service monitor
+kubectl apply -f stats.yaml
+
+# Install HPA objects for vm services
+for s in ${SERVICES[@]}; do
+    kubectl apply -f vm-hpa/$s-hpa.yaml
+done
+
+# # Wait
 # sleep 30
 
 # # Run test
-# kubectl create -f test.yaml
+# kubectl apply -f test.yaml
