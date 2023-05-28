cdcf6b33fd1f20cc75e86a43e70d668f3bd79a83
Jiagan Cheng <chnegjiagan@gmail.com>
2023-05-29 01:41:56 +0800

    [deploy] add pingpong

---
+++ deploy/pingpong/kubernetes/deploy.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# this script is just for reference, please do not run it directly
+
+# apply socialnetwork namespace
+kubectl apply -f namespace.yaml
+
+# Install service config
+kubectl apply -f config.yaml
+
+# Install services
+kubectl apply -f service/ping-service.yaml
+kubectl apply -f service/pong-service.yaml
+
+# Install gateway
+kubectl apply -f gateway.yaml
+
+# Install ServiceMonitor
+kubectl apply -f stats.yaml
