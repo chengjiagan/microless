ad164da481269e4d0d49637aa697d8bec1c06f9d
Jiagan Cheng <chnegjiagan@gmail.com>
2023-05-29 06:26:35 +0800

    [deploy/socialnetwork] update lb deploy

--- deploy/socialnetwork/loadbalancer/deploy.sh
+++ deploy/socialnetwork/loadbalancer/deploy.sh
@@ -2,42 +2,44 @@
 
 # this script is just for reference, please do not run it directly
 
+SERVICES=(post-storage user-timeline user social-graph home-timeline media url-shorten user-mention text compose-post)
+
 # Install socialnetwork namespace
 kubectl apply -f namespace.yaml
 
 # Install service config
 kubectl apply -f config.yaml
-
-SERVICES=(post-storage user-timeline user social-graph home-timeline media url-shorten user-mention text compose-post)
+for s in ${SERVICES[@]}; do
+    kubectl apply -f lb-config/$s-config.yaml
+done
 
 # Install serverless services
 for s in ${SERVICES[@]}; do
     kubectl apply -f serverless-service/$s-service.yaml
+done
+for s in ${SERVICES[@]}; do
+    kubectl apply -f serverless-hpa/$s-hpa.yaml
 done
 
 # Install vm services
 for s in ${SERVICES[@]}; do
     kubectl apply -f vm-service/$s-service.yaml
 done
-
-# Install services
 for s in ${SERVICES[@]}; do
-    kubectl apply -f service/$s-service.yaml
+    kubectl apply -f vm-hpa/$s-hpa.yaml
 done
 
 # Install restful gateway
 kubectl apply -f gateway.yaml
 
 # Install service monitor
+for s in ${SERVICES[@]}; do
+    kubectl apply -f service/$s-service.yaml
+done
 kubectl apply -f stats.yaml
 
-# Install HPA objects for vm services
-for s in ${SERVICES[@]}; do
-    kubectl apply -f vm-hpa/$s-hpa.yaml
-done
+# Install serverless autoscaler
+kubectl apply -f serverless-autoscaler.yaml
 
-# # Wait
-# sleep 30
-
-# # Run test
-# kubectl apply -f test.yaml
+# Install cluster autoscaler
+# kubectl apply -f cluster-autoscaler.yaml
