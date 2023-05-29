61e485e1fd14984f5118bab72356a722cfb9300e
Jiagan Cheng <chnegjiagan@gmail.com>
2023-05-29 21:36:14 +0800

    [deploy/socialnetwork] update k8s deploy

--- deploy/socialnetwork/kubernetes/deploy.sh
+++ deploy/socialnetwork/kubernetes/deploy.sh
@@ -2,44 +2,30 @@
 
 # this script is just for reference, please do not run it directly
 
-# Create socialnetwork namespace
-kubectl create -f namespace.yaml
+SERVICES=(post-storage user-timeline user social-graph home-timeline media url-shorten user-mention text compose-post)
+
+# apply socialnetwork namespace
+kubectl apply -f namespace.yaml
 
 # Install service config
-kubectl create -f config.yaml
+kubectl apply -f config.yaml
 
 # Install services
-kubectl create -f service/post-storage-service.yaml
-kubectl create -f service/user-timeline-service.yaml
-kubectl create -f service/user-service.yaml
-kubectl create -f service/social-graph-service.yaml
-kubectl create -f service/home-timeline-service.yaml
-kubectl create -f service/media-service.yaml
-kubectl create -f service/url-shorten-service.yaml
-kubectl create -f service/user-mention-service.yaml
-kubectl create -f service/text-service.yaml
-kubectl create -f service/compose-post-service.yaml
+for s in ${SERVICES[@]}; do
+    kubectl apply -f service/$s-service.yaml
+done
+for s in ${SERVICES[@]}; do
+    kubectl apply -f hpa/$s-hpa.yaml
+done
 
 # Install gateway
-kubectl create -f gateway.yaml
-
-# Install HPA objects
-kubectl create -f hpa/post-storage-hpa.yaml
-kubectl create -f hpa/user-timeline-hpa.yaml
-kubectl create -f hpa/user-hpa.yaml
-kubectl create -f hpa/social-graph-hpa.yaml
-kubectl create -f hpa/home-timeline-hpa.yaml
-kubectl create -f hpa/media-hpa.yaml
-kubectl create -f hpa/url-shorten-hpa.yaml
-kubectl create -f hpa/user-mention-hpa.yaml
-kubectl create -f hpa/text-hpa.yaml
-kubectl create -f hpa/compose-post-hpa.yaml
+kubectl apply -f gateway.yaml
 
 # Install ServiceMonitor
-kubectl create -f stats.yaml
+kubectl apply -f stats.yaml
 
 # # Wait for services
 # sleep 30
 
 # # Run test
-# kubectl create -f test.yaml
+# kubectl apply -f test.yaml
